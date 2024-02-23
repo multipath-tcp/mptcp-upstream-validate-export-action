5475d77b7a5191bf71512f18361ab6fd5f68f89a
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-02-23 20:52:04 +0100

    shellcheck: fix redirection
    Issues are all reported to stderr.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -688,7 +688,7 @@ _shellcheck() { local dname fname workdir
 	workdir="${PWD}"
 
 	cd "${dname}"
-	/usr/bin/shellcheck -x "${fname}" | tee "${TMPFILE}" >&2
+	/usr/bin/shellcheck -x "${fname}" 2>&1 | tee "${TMPFILE}" >&2
 	{
 		echo -n "  - ${fname}:"
 		if [ -s "${TMPFILE}" ]; then
